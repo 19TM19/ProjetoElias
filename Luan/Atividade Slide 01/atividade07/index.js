96a707a335ab43200f8cfb33015860a4e33c29a6
19TM19 <matheus.benmedito@iesb.edu.br>
2024-08-23 08:46:55 -0300

    Códigos para ADS

---
+++ Luan/Atividade Slide 01/atividade07/index.js
@@ -0,0 +1,13 @@
+const soma = (a,b) => { return a * b }
+console.log(soma(5,7))
+
+const pessoa = 'Matheus'
+const idade = '22'
+const profi = 'Dono de empresa que cria IA (hihi)'
+
+const relatR = {
+    pessoa,
+    idade,
+    profi,
+  };
+console.log(relatR)
